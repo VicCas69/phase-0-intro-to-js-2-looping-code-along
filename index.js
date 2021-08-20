8d08dd8f98307dd106ee33b44ead381fd18188ce
victor castro <victor_castro_01@hotmail.com>
2021-08-20 18:20:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+const names = ["Amarilis","Mom", "Teacher", "Friends"];
+
+function writeCards(name,event){
+    let messages = [];    //Variable to store my new array
+    for(let i=0; i<name.length;i++){
+        console.log(`Thank you, ${name[0]}, for the wonderful ${event} gift!`);
+        messages.push();
+    }
+    return messages;
+}
