aad8a29f7a3e72a20e9d598ee5bae157acfae5b5
victor castro <victor_castro_01@hotmail.com>
2021-08-20 19:24:58 -0400

    Completed assigment

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const names = ["Amarilis","Mom", "Teacher", "Friends"];
 function writeCards(name,event){
     let messages = [];    //Variable to store my new array
     for(let i=0; i<name.length;i++){
-        console.log(`Thank you, ${name[0]}, for the wonderful ${event} gift!`);
-        messages.push();
+        // console.log(`Thank you, ${name[0]}, for the wonderful ${event} gift!`);
+        // debugger;
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+
     }
     return messages;
+}
+
+function countDown(number){
+    //let countDown = [];
+    let i = 0;
+    while (i <= number){
+        console.log(number--)
+    }
+    return number;
 }
